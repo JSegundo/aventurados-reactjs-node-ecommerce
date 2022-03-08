368e681dacd215fcc26218c1e4ad2c0dbc8d664e
AlejoSRodriguez <alejosrodriguez96@gmail.com>
2022-03-08 16:11:32 -0300

    routesV1

--- ecommerce-aventurados/api/routes/index.js
+++ ecommerce-aventurados/api/routes/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const user = require("./user");
+const users = require("./users");
+const products = require("./products");
+const carrito = require('./carrito')
 
-router.use("/user", user);
+router.use("/user", users);
+router.use("/products", products);
+router.use('/carrito', carrito)
 
 module.exports = router;
