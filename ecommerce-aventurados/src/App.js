0d207845c4ff642ce8d1f0b7aac1143a6b16c648
Joacohazee <joacohaze@gmail.com>
2022-03-09 18:45:03 -0300

    content-login

--- ecommerce-aventurados/src/App.js
+++ ecommerce-aventurados/src/App.js
@@ -4,12 +4,15 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Home from "./components/Home";
+
 
 function App() {
   return (
     <>
     <Navbar/>
     <Routes>
+      <Route path='/' element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
     </Routes>
     <Footer/>
--- ecommerce-aventurados/src/components/Login.jsx
+++ ecommerce-aventurados/src/components/Login.jsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
+import '../assets/login.css'
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -78,33 +76,44 @@ const Login = () => {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
+              Inicia con Face..
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: -1, mb: 2 }}
+            >
+              Inicia con Google...
+            </Button>
+            <hr/>
+            <div className='contenedor'>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 3 }}
+            >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 3 }}
+              >
+              Registrate
+            </Button>
+              </div>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
