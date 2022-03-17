bc8aff88c1d7de1130cb80678a37ed4fe4dd5f17
CandelaSU <sanchez.candela@gmail.com>
2022-03-17 10:56:29 -0300

    orders crea

--- ecommerce-aventurados/api/routes/orders.js
+++ ecommerce-aventurados/api/routes/orders.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const order_controllers = require("../controllers/orders.controllers");
-const { getAll } = order_controllers;
+const orders_controllers = require("../controllers/orders.controllers");
+const { getAll ,addOrder} = orders_controllers;
 
 router.get("/:id", getAll);
+router.post("/", addOrder);
 
 module.exports = router;
